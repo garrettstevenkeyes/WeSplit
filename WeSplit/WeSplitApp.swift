d3f8193cf0163083ce0a0684a4a3c580ff81623f
Garrett Keyes <garrett.keyes@gmail.com>
2025-11-02 18:44:07 -0500

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Garrett Keyes on 11/2/25.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
