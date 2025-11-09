3189118e255f8d10ebe4e8f2500c317c168c2e3f
Garrett Keyes <garrett.keyes@gmail.com>
2025-11-09 16:15:09 -0500

    If tip is 0 change foreground

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -63,6 +63,7 @@ struct ContentView: View {
                 
                 Section("Total") {
                     Text(grandTotal, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .foregroundStyle(tipPercentage == 0 ? .red : .primary)
                 }
             }
             .navigationTitle("WeSplit")
